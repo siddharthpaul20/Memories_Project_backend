a73fbf65e6c7845aa110fb1b59e0be8d9eda118c
siddharthpaul20 <siddharthpaul18@gmail.com>
2021-05-06 00:40:52 +0530

    get and post request at backend implemented

---
+++ models/postMessage.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+// const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createAt: {
+        type: Date,
+        default: new Date()
+    }
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema);
+
+// module.exports = { PostMessage };
+export default PostMessage;
